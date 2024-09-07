3cc66140b8aa9f7cf5e16f26ee037a655849c6a5
avieou <gubbinigallery@gmail.com>
2024-09-07 14:22:08 -0700

    adding my two new files

--- module303/src/main/java/org/yuchenou/introductiontojavabasics/WelcomeToJava.java
+++ module303/src/main/java/org/yuchenou/introductiontojavabasics/WelcomeToJava.java
@@ -1,2 +1,27 @@
-package org.yuchenou.introductiontojavabasics;public class WelcomeToJava {
+package org.yuchenou.introductiontojavabasics;
+/*
+Classes in Java should be in UpperCamelCase
+Everything that belongs to the class goes inside of the curly braces
+Our class is public meaning it is accessible to any other classes
+This is a MULTI-LINE COMMENT - it spans lines
+What is a comment - just a note for em or for you
+ */
+public class WelcomeToJava {
+    /*
+    The main method is the entry point our application
+    Public - accessible to any outside classes
+    static - belongs DIRECTLY to the WelcomeToJava
+    void - does not RETURN a value
+    In parentheses we see what the method takes in
+     */
+    public static void main(String[] args) {
+    //When we come back we will learn ONE LINE comments like this
+        /*
+        In Java, we use the System class print methods (println (print line))
+        to display things to the screen or console.
+         */
+    System.out.println("Welcome to Java!!!");
+    System.out.println("And we can write things to the screen!");
+    System.out.println("We will still write the whole thing");
+    }
 }
